3c60d20e873475f6bf74edbe5b1b5d595f342b17
Valentina <valentinaing75@gmail.com>
2020-05-04 08:37:45 -0500

    Documentación

--- src/main/java/edu/unicundi/consultarPago/beans/Index.java
+++ src/main/java/edu/unicundi/consultarPago/beans/Index.java
@@ -17,92 +17,177 @@ import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 
-
 /**
  *
  * @author Valentina
  */
-
 @ManagedBean(name = "index")
 @RequestScoped
-public class Index implements Serializable{
+public class Index implements Serializable {
+
+    /**
+     * la variable nombre almacena el nombre del trabajador
+     */
     private String nombre;
+    /**
+     * la variable apellido almacena el apellido del trabajador
+     */
     private String apellido;
+    /**
+     * la variable cedula almacena el numero de cedula del trabajador
+     */
     private int cedula;
+    /**
+     * la variable fechNacimiento almacena la fecha de nacimiento del trabajador
+     */
     private Date fechaNacimiento;
+    /**
+     * la variable idiomas almacena los idiomas que domina el trabajador
+     */
     private ArrayList idiomas;
+    /**
+     * la variable numeroIdiomas almacena la cantidad de idiomas que domina el
+     * trabajador
+     */
     private int numeroIdiomas;
+    /**
+     * la variable nivelEstudios almacena el nivel de estudio que tiene del trabajador
+     */
     private String nivelEstudios;
+    /**
+     * la variable horasTrabajadas almacena las horas trabajadas
+     */
     private int horasTrabajadas;
 
-    
     public Index() {
     }
-    
+
+    /**
+     * Metodo para mostrar el valor del nombre del trabajador
+     * @return nombre retorna el valor del nombre del trabajador
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Metodo para modificar el valor del nombre del trabajador
+     * @param nombre valor del nombre del trabajador a modificar 
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Metodo para mostrar el valor del apellido del trabajador
+     * @return apellido retorna el valor del nombre del trabajador
+     */
     public String getApellido() {
         return apellido;
     }
 
+    /**
+     * Metodo para modificar el valor del apellido del trabajador
+     * @param apellido valor del apellido del trabajador a modificar 
+     */
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
 
+    /**
+     * Metodo para mostrar el valor de la cedula  del trabajador
+     * @return cedula retorna el valor de la cedula  del trabajador
+     */
     public int getCedula() {
         return cedula;
     }
 
+    /**
+     * Metodo para modificar el valor de la cedula del trabajador
+     * @param cedula valor de la cedula del trabajador a modificar 
+     */
     public void setCedula(int cedula) {
         this.cedula = cedula;
     }
 
+    /**
+     * Metodo para mostrar el valor de la fecha de nacimiento  del trabajador
+     * @return fechaNacimiento retorna el valor de la fecha de nacimiento  del trabajador
+     */
     public Date getFechaNacimiento() {
         return fechaNacimiento;
     }
 
+    /**
+     * Metodo para modificar el valor de la fecha de nacimiento del trabajador
+     * @param fechaNacimiento valor de la fecha de nacimiento del trabajador a modificar 
+     */
     public void setFechaNacimiento(Date fechaNacimiento) {
         this.fechaNacimiento = fechaNacimiento;
     }
 
-    
+    /**
+     * Metodo para mostrar el valor del nivel de estudio del trabajador
+     * @return nivelEstudios retorna el valor del nivel de estudio  del trabajador
+     */
     public String getNivelEstudios() {
         return nivelEstudios;
     }
 
+    /**
+     * Metodo para modificar el valor del nivel de estudio del trabajador
+     * @param nivelEstudios valor del nivel de estudio del  trabajador a modificar 
+     */
     public void setNivelEstudios(String nivelEstudios) {
         this.nivelEstudios = nivelEstudios;
     }
 
+    /**
+     * Metodo para mostrar los idiomas que domina el trabajador
+     * @return idiomas retorna el array  de idiomas que domina el trabajador
+     */
     public ArrayList getIdiomas() {
         return idiomas;
     }
 
+    /**
+     * Metodo para modificar el valor de isiomas que domina el trabajador
+     * @param idiomas valor de idiomas que domina el trabajador a modificar 
+     */
     public void setIdiomas(ArrayList idiomas) {
         this.idiomas = idiomas;
     }
 
+    /**
+     * Metodo para mostrar el numero de idiomas que domina el trabajador
+     * @return numeroIdiomas retorna el valor de numero  idiomas que domina el trabajador
+     */
     public int getNumeroIdiomas() {
         return numeroIdiomas;
     }
 
+    /**
+     * Metodo para modificar el valor del numero de idiomas que domina el trabajador
+     * @param numeroIdiomas valor del numero de idiomas que domina el trabajador a modificar 
+     */
     public void setNumeroIdiomas(int numeroIdiomas) {
         this.numeroIdiomas = numeroIdiomas;
     }
-    
+
+    /**
+     * Metodo para mostrar las horas trabajadas
+     * @return horasTrabajadas retorna el valor de las horas trabajadas 
+     */
     public int getHorasTrabajadas() {
         return horasTrabajadas;
     }
 
+    /**
+     * Metodo para modificar el valor de horas trabajados
+     * @param horasTrabajadas valor de horas trabajadas  a modificar 
+     */
     public void setHorasTrabajadas(int horasTrabajadas) {
         this.horasTrabajadas = horasTrabajadas;
     }
 
-   
 }
--- src/main/java/edu/unicundi/consultarPago/beans/Pago.java
+++ src/main/java/edu/unicundi/consultarPago/beans/Pago.java
@@ -16,24 +16,58 @@ import java.text.SimpleDateFormat;
 
 /**
  *
- * @author Valentina
+ * @author Luz
  */
 @ManagedBean(name = "pago")
 @RequestScoped
 public class Pago implements Serializable {
 
     @ManagedProperty("#{index}")
+    /**
+     * la variable informacion trae todos los datos del formulario 
+     */
     private Index informacion;
-
+    /**
+     * la variable nombre almacena el nombre del trabajador
+     */
     private String nombre;
+    /**
+     * la variable apellido almacena el apellido del trabajador
+     */
     private String apellido;
+    /**
+     * la variable cedula almacena el numero de cedula del trabajador
+     */
     private int cedula;
+    /**
+     * la variable fechNacimiento almacena la fecha de nacimiento del trabajador
+     */
     private Date fechaNacimiento;
+    /**
+     * la variable nivelEstudios almacena el nivel de estudio que tiene del
+     * trabajador
+     */
     private String nivelEstudios;
+    /**
+     * la variable idiomas almacena los idiomas que domina el trabajador
+     */
     private ArrayList idiomas;
+    /**
+     * la variable pagoTotal almacena el valor del pago total del trabajador
+     */
     private int pagoTotal;
+    /**
+     * la variable sueldoBase almacena el valor del sueldo base del trabajador
+     */
     private int sueldoBase;
+    /**
+     * la variable valorIdioma almacena el valor por cada idioma que domine el
+     * trabajador
+     */
     private int valorIdioma;
+    /**
+     * la variable horasTrabajadas almacena el numero de horfas trabajadas
+     */
     private int horasTrabajadas;
 
     public Pago() {
@@ -58,7 +92,7 @@ public class Pago implements Serializable {
     /**
      * Metodo que calcula cuanto dinero se debe sumar segun el nivel academico
      *
-     * @return
+     * @return total valor deacuerdo al nivel academico
      */
     private int totalNivelAcademico() {
         int total;
@@ -87,7 +121,7 @@ public class Pago implements Serializable {
      * Metodo que calcula cuanto dinero se debe sumar segun la cantidad de
      * idiomas
      *
-     * @return
+     * @return valorIdioma el valor total deacuerdo a los idiomas que domine
      */
     private int totalIdioma() {
         int cantidadIdiomas, valorIdiomas;
@@ -111,90 +145,182 @@ public class Pago implements Serializable {
 
     }
 
+    /**
+     * Metodo para mostrar los valores que se ingresaron en el formulario
+     * @return informacion retorna los valores ingresados en el formulario 
+     */
     public Index getInformacion() {
         return informacion;
     }
 
+    /**
+     * Metodo para modificar los valores que se ingresaron en el formulario
+     * @param informacion valores ingresados en el forumulario a modificar
+     */
     public void setInformacion(Index informacion) {
         this.informacion = informacion;
     }
 
+    /**
+     * Metodo para mostrar el valor del nombre del trabajador
+     * @return nombre retorna el valor del nombre del trabajador
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Metodo para modificar el valor del nombre del trabajador
+     * @param nombre valor del nombre del trabajador a modificar
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Metodo para mostrar el valor del apellido del trabajador
+     * @return apellido retorna el valor del nombre del trabajador
+     */
     public String getApellido() {
         return apellido;
     }
 
+    /**
+     * Metodo para modificar el valor del apellido del trabajador
+     * @param apellido valor del apellido del trabajador a modificar
+     */
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
 
+    /**
+     * Metodo para mostrar el valor de la cedula del trabajador
+     * @return cedula retorna el valor de la cedula del trabajador
+     */
     public int getCedula() {
         return cedula;
     }
 
+    /**
+     * Metodo para modificar el valor de la cedula del trabajador
+     * @param cedula valor de la cedula del trabajador a modificar
+     */
     public void setCedula(int cedula) {
         this.cedula = cedula;
     }
 
+    /**
+     * Metodo para mostrar el valor del nivel de estudio del trabajador
+     * @return nivelEstudios retorna el valor del nivel de estudio del
+     * trabajador
+     */
     public String getNivelEstudios() {
         return nivelEstudios;
     }
 
+    /**
+     * Metodo para modificar el valor del nivel de estudio del trabajador
+     * @param nivelEstudios valor del nivel de estudio del trabajador a
+     * modificar
+     */
     public void setNivelEstudios(String nivelEstudios) {
         this.nivelEstudios = nivelEstudios;
     }
 
+    /**
+     * Metodo para mostrar los idiomas que domina el trabajador
+     * @return idiomas retorna el array de idiomas que domina el trabajador
+     */
     public ArrayList getIdiomas() {
         return idiomas;
     }
 
+    /**
+     * Metodo para modificar el valor de isiomas que domina el trabajador
+     * @param idiomas valor de idiomas que domina el trabajador a modificar
+     */
     public void setIdiomas(ArrayList idiomas) {
         this.idiomas = idiomas;
     }
 
+    /**
+     * Metodo para mostrar el valor del pago total del trabajador
+     * @return sueldoBase retorna el valor del pago total del trabajador
+     */
     public int getPagoTotal() {
         return pagoTotal;
     }
 
+    /**
+     * Metodo para modificar el valor del pago total del trabajador
+     * @param pagoTotal valor del pago totale del trabajador a modificar
+     */
     public void setPagoTotal(int pagoTotal) {
         this.pagoTotal = pagoTotal;
     }
 
+    /**
+     * Metodo para mostrar el valor del sueldo base del trabajador
+     * @return sueldoBase retorna el valor del sueldo base del trabajador
+     */
     public int getSueldoBase() {
         return sueldoBase;
     }
 
+    /**
+     * Metodo para modificar el valor del sueldo base del trabajador
+     * @param sueldoBase valor del sueldo base del trabajador a modificar
+     */
     public void setSueldoBase(int sueldoBase) {
         this.sueldoBase = sueldoBase;
     }
 
+    /**
+     * Metodo para mostrar el valor de la fecha de nacimiento del trabajador
+     * @return fechaNacimiento retorna el valor de la fecha de nacimiento del
+     * trabajador
+     */
     public Date getFechaNacimiento() {
         return fechaNacimiento;
     }
 
+    /**
+     * Metodo para modificar el valor de la fecha de nacimiento del trabajador
+     * @param fechaNacimiento valor de la fecha de nacimiento del trabajador a
+     * modificar
+     */
     public void setFechaNacimiento(Date fechaNacimiento) {
         this.fechaNacimiento = fechaNacimiento;
     }
 
+    /**
+     * Metodo para mostrar las horas trabajadas
+     * @return horasTrabajadas retorna el valor de las horas trabajadas
+     */
     public int getHorasTrabajadas() {
         return horasTrabajadas;
     }
 
+    /**
+     * Metodo para modificar el valor de horas trabajados
+     * @param horasTrabajadas valor de horas trabajadas a modificar
+     */
     public void setHorasTrabajadas(int horasTrabajadas) {
         this.horasTrabajadas = horasTrabajadas;
     }
 
+    /**
+     * Metodo para mostrar el valor a pagar por los idiomas
+     * @return valorIdioma retorna el valor a pagar de los idiomas
+     */
     public int getValorIdioma() {
         return valorIdioma;
     }
 
+    /**
+     * Metodo para modificar el valor a pagar por los idiomas
+     * @param valorIdioma valor a pagar por los idiomas a modificar
+     */
     public void setValorIdioma(int valorIdioma) {
         this.valorIdioma = valorIdioma;
     }
